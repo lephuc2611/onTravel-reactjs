c624bfec13ba919caa2479e15b6453647207d779
Ngô Vưn Nam <anh02284@gmail.com>
2022-06-05 21:09:27 +0700

    ui: footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../header/logo/Logo";
+import "./Footer.css";
 export default function Footer() {
-   return (
-      <footer>
-         footer
-      </footer>
-   )
+  const [logoColor, setlogoColor] = useState("#008489");
+  return (
+    <footer>
+      <div className="footer-icon">
+        <Logo color={logoColor} />
+      </div>
+      <div className="footer-navbars">
+        <ul className="footer-navbars-item">
+          <NavLink to="/hotels" className="footer-navbars-link">
+            Hotels
+          </NavLink>
+          <NavLink to="/listing" className="footer-navbars-link">
+            Listing
+          </NavLink>
+          <NavLink to="/pricing" className="footer-navbars-link">
+            Pricing
+          </NavLink>
+          <NavLink to="/hotels" className="footer-navbars-link">
+            Privacy
+          </NavLink>
+          <NavLink to="/agent" className="footer-navbars-link">
+            Agent
+          </NavLink>
+        </ul>
+      </div>
+      <div className="footer-source">Copyright @ 2022 RedQ, Inc.</div>
+    </footer>
+  );
 }
