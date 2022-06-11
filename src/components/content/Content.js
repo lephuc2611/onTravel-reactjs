3340f0360b8e45c57b61cd39f0d1b065ea2c630a
Ngô Vưn Nam <anh02284@gmail.com>
2022-06-11 17:01:30 +0700

    ui: add component in home

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -2,12 +2,15 @@ import React from "react";
 import Banner from "./banner/Banner";
 import Explore from "./explore/Explore";
 import "./Content.css";
+import ProductsHome from "./ProductsHome/ProductsHome";
 
 export default function Content() {
   return (
     <main>
       <Banner />
       <Explore />
+      <ProductsHome heading={"Travelers’ Choice: Top hotels"} />
+      <ProductsHome heading={"Best Rated: Luxary hotels"} />
     </main>
   );
 }
