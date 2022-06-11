63d89e0ec3295ca895e73dc40b53350f35eb53a8
Ngô Vưn Nam <anh02284@gmail.com>
2022-06-11 16:38:47 +0700

    ui: explore

--- src/components/content/explore/Explore.js
+++ src/components/content/explore/Explore.js
@@ -1,13 +1,144 @@
 import React from "react";
-import jsonExplore from "../../../assets/data/explore.json";
-import Heading from "../heading/Heading";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import "./Explore.css";
+import Heading from "../heading/Heading";
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+    slidesToSlide: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5,
+    slidesToSlide: 5,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 export default function Explore() {
   return (
     <div className="main-explore">
-       <Heading href={'#'} heading={"Explore Destinations"} />
-      <div className="explore-carousel">{JSON.stringify(jsonExplore)}</div>
+      <Heading href={"#"} heading={"Explore Destinations"} />
+      <Carousel responsive={responsive}>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+        <div className="explore-box">
+          <a>
+            <img
+              className="explore-box-imgage"
+              src="http://s3.amazonaws.com/redqteam.com/tripfinder-images/cairo.jpg"
+            />
+            <div className="explore-box-item">
+              <h3 className="explore-box-item__title">Prague</h3>
+              <p className="explore-box-item__hotels">55556 Hotels</p>
+            </div>
+          </a>
+        </div>
+      </Carousel>
     </div>
   );
 }
